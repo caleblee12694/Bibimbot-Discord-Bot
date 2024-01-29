fc99fb089d9c2b648b3961c583b1781032927cc5
caleblee12694 <caleblee12694@gmail.com>
2024-01-29 12:14:52 -0500

    First Push

---
+++ main.py
@@ -0,0 +1,8 @@
+from typing import Final
+import os
+from dotenv import load_dotenv
+from discord import Intents, Client, Message
+from responses import get_response
+
+load_dotenv()
+TOKEN: Final[str] = os.getenv('DISCORD_TOKEN')
---
+++ responses.py
@@ -0,0 +1,2 @@
+def get_response(user_input: str) -> str:
+    raise NotImplementedError('Code is missing...')
