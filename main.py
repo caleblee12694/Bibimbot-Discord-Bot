7bd8670895db20d6da9e84a083057fd5551bde80
caleblee12694 <caleblee12694@gmail.com>
2024-01-29 12:41:19 -0500

    Added basic functionality

--- main.py
+++ main.py
@@ -4,5 +4,50 @@ from dotenv import load_dotenv
 from discord import Intents, Client, Message
 from responses import get_response
 
+# Loading Token
 load_dotenv()
 TOKEN: Final[str] = os.getenv('DISCORD_TOKEN')
+
+# Bot Setup
+intents: Intents = Intents.default()
+intents.message_content = True
+client: Client = Client(intents=intents)
+
+# Message functionality
+async def send_message(message: Message, user_message: str) -> None:
+    if not user_message:
+        print('(Message was empty because intents were not enabled)')
+        return
+    
+    if is_private := user_message[0] == '?':
+        user_message = user_message[1:]
+
+    try:
+        response: str = get_response(user_message)
+        await message.author.send(response) if is_private else message.channel.send(response)
+    except Exception as e:
+        print(e)
+
+# Startup Handling
+@client.event
+async def on_read() -> None:
+    print(f'{client.user} is not running!')
+
+# Incoming Messages Handling
+@client.event
+async def on_message(message: Message) -> None:
+    if message.author == client.user:
+        return
+    
+    username: str = str(message.author)
+    user_message: str = message.content
+    channel: str = str(message.channel)
+
+    print(f'[{channel}] {username}: "{user_message}')
+    await send_message(message, user_message)
+
+def main() -> None:
+    client.run(token=TOKEN)
+
+if __name__ == '__main__':
+    main()
--- responses.py
+++ responses.py
@@ -1,2 +1,13 @@
+from random import choice, randint
+
 def get_response(user_input: str) -> str:
-    raise NotImplementedError('Code is missing...')
+    lowered: str = user_input.lower()
+
+    if lowered == '':
+        return 'SAY SOMETHING.'
+    elif 'hello' in lowered:
+        return 'haiii >.<'
+    else:
+        return choice(['wat...?',
+                       'I dunno wym',
+                       'ok...'])
